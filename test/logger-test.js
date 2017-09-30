0c0d9cd67c9f2244265fe7e3dfd503841dcaec18
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-29 21:28:54 -0400

    Improve Logger tests

--- test/logger-test.js
+++ test/logger-test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import test from 'tape'
 import Logger from '../src/logger'
 
@@ -26,56 +27,70 @@ const logB = {
   },
 }
 
-Log.log(logA.log, logA.data)
-Log.log(logB.log, logB.data)
-
-test('MayoLog is instantiated', t => {
+test('Logger object is instantiated', t => {
   t.isEqual(typeof Log, 'object')
   t.end()
 })
 
-test('Records the start time', t => {
+test('Logger object records the start time', t => {
   t.isEqual(startTime.toString(), Log.startTime.toString())
   t.end()
 })
 
-test('Outputs to console', t => {
+test('log() records the entry', t => {
+  Log.log(logA.log, logA.data)
+  Log.log(logB.log, logB.data)
+  t.isEqual(Log.logEntries.length, 2)
   t.end()
 })
 
-test('Returns a log output', t => {
+test('log() records the time of log entry', t => {
+  t.isEqual(typeof Log.logEntries[0].time, 'number')
+  t.end()
+})
+
+test('log() records the log of the entry', t => {
+  t.isEqual(typeof Log.logEntries[0].log, 'string')
+  t.end()
+})
+test('log() records the data of the entry', t => {
+  t.isEqual(typeof Log.logEntries[0].data, 'object')
+  t.end()
+})
+
+test('toConsole() returns an array of the output', t => {
   const actualOutput = Log.toConsole()
   t.isEqual(actualOutput.length, 2)
   t.end()
 })
 
-test('Finds the key in the object', t => {
+test('findWithDataAttribute() finds the key in the object', t => {
   const found = Log.findWithDataAttribute('keyA')
   t.isEqual(found.length, 1)
   t.isEqual(found[0].log, 'Log - A')
   t.end()
 })
 
-test('Returns an empty array if the key is not found in the object', t => {
+test('findWithDataAttribute() returns an empty array if the key is not found', t => {
   const found = Log.findWithDataAttribute('keyX')
   t.isEqual(found.length, 0)
   t.end()
 })
 
-test('Finds all entries with the key', t => {
+test('findWithDataAttribute() finds all entries with the key', t => {
   const found = Log.findWithDataAttribute('key')
   t.isEqual(found.length, 2)
   t.end()
 })
 
-test('Finds the nested key in the object', t => {
+test('findWithDataAttribute() finds the nested key in the object', t => {
   const found = Log.findWithDataAttribute('nestedKey')
   t.isEqual(found.length, 1)
   t.isEqual(found[0].log, 'Log - B')
   t.end()
 })
 
-test('Finds deeply nested key in the object', t => {
+test('findWithDataAttribute() finds deeply nested key in the object', t => {
   const found = Log.findWithDataAttribute('deepNestedKey')
   t.isEqual(found.length, 1)
   t.isEqual(found[0].log, 'Log - B')
